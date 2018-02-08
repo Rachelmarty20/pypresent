3310bbde3daa84a629c6b43167b4794eba6b597a
Rachelmarty20 <Rachelmarty20@gmail.com>
2018-02-08 13:43:20 -0800

    Updated for preliminary allele-specific test

--- affinities/MHCI.py
+++ affinities/MHCI.py
@@ -2,13 +2,42 @@
 Calls affinity prediction tools for MHC-I
 
 All output should be in the form of a dataframe with the following columns:
-peptide, allele, rank
+peptide, rank
 
 '''
 
+import os
+import time
+import pandas as pd
+
 NETMHCPAN30_PATH = '/cellar/users/ramarty/programs/netMHCpan-3.0/netMHCpan'
+TEMP_DIR = '/cellar/users/ramarty/Data/pypresent/tmp/'
 
-def run_netmhcpan30(mutation, allele)
 
+def run_netmhcpan30(allele, mutation):
+    """
+    To run netMHCpan3.0 and return a cleaned dataframe
+    :param allele: an Allele object
+    :param mutation: a Mutation object
+    :return: A cleaned output dataframe path
+    """
+    # create unique identifier
+    raw_affinities_file = '{0}raw_affinities.{1}_{2}'.format(TEMP_DIR, mutation.id,
+                                                         str(time.time()).split('.')[0])
 
-# HLA-C14:09
+    # Run command
+    cmd = '{0}/netMHCpan -a {1} -f {2} + -xls -xlsfile ' \
+          + '{3}'.format(NETMHCPAN30_PATH, allele.id, mutation.restricted_fasta_file,
+                         raw_affinities_file)
+    os.system(cmd)
+
+    # Alter output file
+    df = pd.read_csv('/cellar/users/ramarty/Projects/pypresent/data/test.netmhcpan.output.csv', sep='\t',
+               header=1)
+    df = df[['Peptide', 'Rank']]
+
+    # Save re-formatted output file
+    formatted_affinities_file = '{0}formatted_affinities.{1}_{2}'.format(TEMP_DIR, mutation.id,
+                                                         str(time.time()).split('.')[0])
+    df.to_csv(formatted_affinities_file)
+    return formatted_affinities_file
--- scoring/single.py
+++ scoring/single.py
@@ -1,18 +1,21 @@
 import pandas as pd
 
-# Utility Functions
 
-
-
-def best_rank(affinity_data_file, mutation):
+def best_rank(affinity_data_file, mutation, MHC_class='I'):
     """
-
-    :param affinity_data_file:
+    Import a dataframe with two columns: Peptide and Rank.
+    Return the minimum rank containing the residue.
+    :param affinity_data_file: location of file with affinity data
     :return: A best rank score
     """
 
     # Import data frame
+    df = pd.read_csv(affinity_data_file, index_col=0)
 
     # Restrict to peptides containing mutation
+    peptides = mutation.get_peptides_containing_residue(MHC_class)
+    df = df[df.Peptide.isin(peptides)]
 
     # Return the minimum
+    return min(df.Rank)
+
