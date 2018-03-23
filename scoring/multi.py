e82b723674fde816580a11a955907710c98bca8c
Rachelmarty20 <Rachelmarty20@gmail.com>
2018-03-23 09:45:41 -0700

    udated scoring methods

--- scoring/multi.py
+++ scoring/multi.py
@@ -1,3 +1,4 @@
+import scipy.stats as sp
 
 
 def best_rank(allele_scores):
@@ -9,3 +10,26 @@ def best_rank(allele_scores):
 
     # Return the minimum
     return min(allele_scores)
+
+
+def harmonic_mean(allele_scores):
+    """
+    Determine the harmonic mean score from the patient's alleles.
+    :param allele_scores: List of scores for patient's six alleles
+    :return: A harnmonic mean rank score
+    """
+
+    # Return the harmonic mean
+    return sp.hmean(allele_scores)
+
+
+def geometric_mean(allele_scores):
+    """
+    Determine the geometric mean score from the patient's alleles.
+    :param allele_scores: List of scores for patient's six alleles
+    :return: A geometric mean score
+    """
+
+    # Return the harmonic mean
+    return sp.mstats.gmean(allele_scores)
+
--- scoring/single.py
+++ scoring/single.py
@@ -1,5 +1,23 @@
 import pandas as pd
+import scipy.stats as sp
 
+
+def _import_dataframe(affinity_data_file, mutation, mhc_class='I'):
+    """
+    Import a dataframe with two columns: Peptide and Rank.
+    :param affinity_data_file:
+    :param mutation:
+    :param mhc_class:
+    :return:
+    """
+
+    # Import data frame
+    df = pd.read_csv(affinity_data_file, index_col=0)
+
+    # Restrict to peptides containing mutation
+    peptides = mutation.get_peptides_containing_residue(mhc_class)
+    df = df[df.Peptide.isin(peptides)]
+    return df
 
 def best_rank(affinity_data_file, mutation, mhc_class='I'):
     """
@@ -9,13 +27,21 @@ def best_rank(affinity_data_file, mutation, mhc_class='I'):
     :return: A best rank score
     """
 
-    # Import data frame
-    df = pd.read_csv(affinity_data_file, index_col=0)
-
-    # Restrict to peptides containing mutation
-    peptides = mutation.get_peptides_containing_residue(mhc_class)
-    df = df[df.Peptide.isin(peptides)]
+    df = _import_dataframe(affinity_data_file, mutation, mhc_class)
 
     # Return the minimum
     return min(df.Rank)
 
+
+def harmonic_mean(affinity_data_file, mutation, mhc_class='I'):
+    """
+    Import a dataframe with two columns: Peptide and Rank.
+    Return the harmonic mean rank containing the residue.
+    :param affinity_data_file: location of file with affinity data
+    :return: A best rank score
+    """
+
+    df = _import_dataframe(affinity_data_file, mutation, mhc_class)
+
+    # Return the minimum
+    return sp.hmean(df.Rank)
