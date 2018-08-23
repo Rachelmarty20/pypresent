153a102c9044cc26cfa761e23e2f77aac9021223
Rachelmarty20 <Rachelmarty20@gmail.com>
2018-08-23 16:27:54 -0700

    moved tmp directory

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ NETMHCPAN30_PATH = '/cellar/users/ramarty/programs/netMHCpan-3.0/netMHCpan'
 NETMHCIIPAN31_PATH = '/cellar/users/ramarty/programs/netMHCIIpan-3.1/netMHCIIpan'
 
 # Update based on your desired tmp directory
-TEMP_DIR = '/cellar/users/ramarty/Data/pypresent/tmp/'
+TEMP_DIR = '/tmp/ramarty/'
 
 
 def setup_MHCI():
