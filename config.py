0a52b69288f1b2514d7c38305d1810b8da822193
Rachelmarty20 <Rachelmarty20@gmail.com>
2018-10-18 18:39:25 -0700

    changing to netMHCpan40

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 # Update these paths according to your netMHCpan/netMHCIIpan downloads
 NETMHCPAN30_PATH = '/cellar/users/ramarty/programs/netMHCpan-3.0/netMHCpan'
+NETMHCPAN40_PATH = '/cellar/users/ramarty/programs/netMHCpan-4.0/netMHCpan'
 NETMHCIIPAN31_PATH = '/cellar/users/ramarty/programs/netMHCIIpan-3.1/netMHCIIpan'
 
 # Update based on your desired tmp directory
@@ -7,7 +8,7 @@ TEMP_DIR = '/tmp/ramarty/'
 
 
 def setup_MHCI():
-    return NETMHCPAN30_PATH, TEMP_DIR
+    return NETMHCPAN40_PATH, TEMP_DIR
 
 def setup_MHCII():
     return NETMHCIIPAN31_PATH, TEMP_DIR
